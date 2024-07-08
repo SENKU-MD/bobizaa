b984fa2ccfe4e1df4a1036a436520e14b8f28415
SENKU-MD <hjwjuhshsv@gmail.com>
2024-07-08 15:38:16 +0100

    Update Dragon-ball-z.js

--- plugins/Dragon-ball-z.js
+++ plugins/Dragon-ball-z.js
@@ -33,5 +33,5 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['dragon-ball-z']
 handler.tags = ['anime']
-handler.command = /^dragon-ball-z$/i
+handler.command = /^دراغون_بال$/i
 export default handler
